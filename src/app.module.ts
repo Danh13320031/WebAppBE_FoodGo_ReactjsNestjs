4dec7b2016541b6c46465a82d8b96a6e5ccbeef0
Danh13320031 <danh13320031@gmail.com>
2025-12-30 16:12:15 +0700

    #configuration: Successfully connected to the postgre sql database.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { sequelizeConfig } from './configs/sequelize.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    SequelizeModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: sequelizeConfig,
+    }),
+  ],
 })
 export class AppModule {}
