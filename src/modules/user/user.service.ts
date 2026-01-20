f9348488f928ee54239c61082e49c708b9938066
Danh13320031 <danh13320031@gmail.com>
2026-01-20 23:22:01 +0700

    #user-service: Successfully created/registered a user account.

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,6 +1,9 @@
+import { SALT_ROUNDS } from '@/common/constants';
 import { User } from '@/models';
 import { BadGatewayException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
+import * as bcrypt from 'bcrypt';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UserService {
@@ -19,5 +22,24 @@ export class UserService {
     console.log(alreadyExists);
 
     return { message: 'Login' };
+  }
+
+  async register(createUserDto: CreateUserDto) {
+    const alreadyExists = await this.findByEmail(createUserDto.email);
+
+    if (alreadyExists) throw new BadGatewayException('Email đã được đăng ký');
+
+    const hashPassword: string = bcrypt.hashSync(
+      createUserDto.password,
+      SALT_ROUNDS,
+    );
+    const payload = {
+      ...createUserDto,
+      password: hashPassword,
+    };
+
+    await this.userModel.create(payload as any);
+
+    return { message: 'Đăng ký tài khoản thành công' };
   }
 }
