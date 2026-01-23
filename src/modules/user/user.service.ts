d5d142bb0da180b389b4bdc40411c864ea1f9e93
Danh13320031 <danh13320031@gmail.com>
2026-01-23 23:11:34 +0700

    #auth-service: Successfully coded the basic login featurewith jwt token.

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,53 +1,48 @@
-import { SALT_ROUNDS } from '@/common/constants';
 import { User } from '@/models';
 import { BadGatewayException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { InjectModel } from '@nestjs/sequelize';
-import * as bcript from 'bcrypt';
 import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UserService {
-  constructor(@InjectModel(User) private readonly userModel: typeof User) {}
+  constructor(
+    @InjectModel(User) private readonly userModel: typeof User,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async findByEmail(email: string) {
-    return await this.userModel.findOne({ where: { email }, raw: true });
+    return await this.userModel.findOne({ where: { email } });
   }
 
   async validateUser(email: string, password: string) {
-    const alreadyExists = await this.findByEmail(email);
+    const user = await this.findByEmail(email);
 
-    if (!alreadyExists)
-      throw new BadGatewayException('Người dùng không tồn tại');
+    if (!user) throw new BadGatewayException('Người dùng không tồn tại');
 
-    const isCorrectPassword = bcript.compareSync(
-      password,
-      alreadyExists.password,
-    );
+    const isCorrectPassword = user.comparePassword(password);
 
     if (!isCorrectPassword)
       throw new BadGatewayException('Mật khẩu không chính xác');
 
-    const { password: _, ...result } = alreadyExists;
-
     // Trả về access token hoặc thông tin người dùng
-    return { message: 'Đăng nhập thành công', data: result };
+    const plainUser = user.getUserWithoutPassword();
+    const accessToken = await this.jwtService.signAsync({
+      uid: plainUser.id,
+      role: plainUser.role,
+    });
+
+    return {
+      message: 'Đăng nhập thành công',
+      accessToken,
+    };
   }
 
   async register(createUserDto: CreateUserDto) {
-    const alreadyExists = await this.findByEmail(createUserDto.email);
+    const user = await this.findByEmail(createUserDto.email);
 
-    if (alreadyExists) throw new BadGatewayException('Email đã được đăng ký');
-
-    const hashPassword: string = bcript.hashSync(
-      createUserDto.password,
-      SALT_ROUNDS,
-    );
-    const payload = {
-      ...createUserDto,
-      password: hashPassword,
-    };
-
-    await this.userModel.create(payload as any);
+    if (user) throw new BadGatewayException('Email đã được đăng ký');
+    await this.userModel.create(createUserDto as any);
 
     return { message: 'Đăng ký tài khoản thành công' };
   }
